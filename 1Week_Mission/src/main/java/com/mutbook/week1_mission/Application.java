d1cc051c87aa52fcaaced4ab6fbe37295469c933
heokyeongju <beewt@naver.com>
2022-10-17 13:31:12 +0900

    chore: yml파일을 이용한  기초 세팅 추가

---
+++ 1Week_Mission/src/main/java/com/mutbook/week1_mission/Application.java
@@ -0,0 +1,13 @@
+package com.mutbook.week1_mission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
