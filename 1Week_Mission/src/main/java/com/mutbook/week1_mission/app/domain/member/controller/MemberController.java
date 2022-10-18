aa8561406a41a05dd82171f784c2703b081a833e
heokyeongju <beewt@naver.com>
2022-10-18 15:21:12 +0900

    feat: MemberController 작성 #3
    회원가입 기능 구현을 위한 controller와 JoinDto를 작성

---
+++ 1Week_Mission/src/main/java/com/mutbook/week1_mission/app/domain/member/controller/MemberController.java
@@ -0,0 +1,34 @@
+package com.mutbook.week1_mission.app.domain.member.controller;
+
+import com.mutbook.week1_mission.app.domain.member.dto.JoinDto;
+import com.mutbook.week1_mission.app.domain.member.service.MemberService;
+import com.mutbook.week1_mission.util.Util;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+    private final MemberService memberService;
+
+    @PreAuthorize("isAnonymous()")
+    @GetMapping("/join")
+    public String showJoin() {
+        return "member/join";
+    }
+
+    @PreAuthorize("isAnonymous()")
+    @PostMapping("/join")
+    public String join(@Valid JoinDto joinDto) {
+        memberService.join(joinDto.getUsername(), joinDto.getPassword(), joinDto.getEmail());
+
+        return "redirect:/member/login?msg=" + Util.url.encode("회원가입이 완료되었습니다.");
+    }
+}
