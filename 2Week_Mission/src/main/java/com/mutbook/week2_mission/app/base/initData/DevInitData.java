127d3ea0d443c277d05b54ec526c77409e0baf07
heokyeongju <beewt@naver.com>
2022-10-25 23:33:53 +0900

    test: 테스트 데이터 추가

--- 2Week_Mission/src/main/java/com/mutbook/week2_mission/app/base/initData/DevInitData.java
+++ 2Week_Mission/src/main/java/com/mutbook/week2_mission/app/base/initData/DevInitData.java
@@ -2,6 +2,7 @@ package com.mutbook.week2_mission.app.base.initData;
 
 import com.mutbook.week2_mission.app.domain.member.service.MemberService;
 import com.mutbook.week2_mission.app.domain.post.service.PostService;
+import com.mutbook.week2_mission.app.domain.product.service.ProductService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +14,10 @@ public class DevInitData implements InitDataBefore{
     @Bean
     CommandLineRunner initData(
             MemberService memberService,
-            PostService postService) {
+            PostService postService,
+            ProductService productService) {
         return args -> {
-            before(memberService, postService);
+            before(memberService, postService, productService);
         };
     }
 }
