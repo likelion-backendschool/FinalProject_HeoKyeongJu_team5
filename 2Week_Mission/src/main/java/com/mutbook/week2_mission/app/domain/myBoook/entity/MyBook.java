3081aa19648c9bfdc47042efdb24b78604a5a03b
heokyeongju <beewt@naver.com>
2022-10-26 17:30:40 +0900

    feat: myBook entity 작성 #10

---
+++ 2Week_Mission/src/main/java/com/mutbook/week2_mission/app/domain/myBoook/entity/MyBook.java
@@ -0,0 +1,30 @@
+package com.mutbook.week2_mission.app.domain.myBoook.entity;
+
+import com.mutbook.week2_mission.app.base.entity.BaseEntity;
+import com.mutbook.week2_mission.app.domain.member.entity.Member;
+import com.mutbook.week2_mission.app.domain.product.entity.Product;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+@Table(name = "my_book")
+public class MyBook extends BaseEntity {
+    @ManyToOne(fetch = LAZY)
+    private Member member;
+    @ManyToOne(fetch = LAZY)
+    private Product product;
+}
