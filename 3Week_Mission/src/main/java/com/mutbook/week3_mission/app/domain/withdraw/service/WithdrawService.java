ce4f1caabf61511b392eeac1d1caa534aa978550
heokyeongju <beewt@naver.com>
2022-11-02 13:45:35 +0900

    feat: 출금 가능 여부 로직 추가 #15

--- 3Week_Mission/src/main/java/com/mutbook/week3_mission/app/domain/withdraw/service/WithdrawService.java
+++ 3Week_Mission/src/main/java/com/mutbook/week3_mission/app/domain/withdraw/service/WithdrawService.java
@@ -1,9 +1,8 @@
 package com.mutbook.week3_mission.app.domain.withdraw.service;
 
 import com.mutbook.week3_mission.app.base.rq.Rq;
-import com.mutbook.week3_mission.app.domain.member.entity.AuthLevel;
 import com.mutbook.week3_mission.app.domain.member.entity.Member;
-import com.mutbook.week3_mission.app.domain.rebate.entity.RebateOrderItem;
+import com.mutbook.week3_mission.app.domain.withdraw.entity.ApplyStatus;
 import com.mutbook.week3_mission.app.domain.withdraw.entity.Withdraw;
 import com.mutbook.week3_mission.app.domain.withdraw.repository.WithdrawRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,12 +18,20 @@ public class WithdrawService {
     private final WithdrawRepository withdrawRepository;
     private final Rq rq;
     public Withdraw apply(String accountNumber, String bank, long withdrawAmount){
-        // 예치금보다 작아야한다는 로직 추가해야 함.
+        Member member = rq.getMember();
+        long cash = member.getCash();
+        long amount = withdrawAmount;
+
+        if (amount > cash) {
+            throw new RuntimeException("예치금이 부족합니다.");
+        }
+
         Withdraw withdrawInfo = Withdraw.builder()
                 .accountNumber(accountNumber)
                 .bank(bank)
                 .withdrawAmount(withdrawAmount)
                 .member(rq.getMember())
+                .applyStatus(ApplyStatus.READY_STATUS)
                 .build();
 
         withdrawRepository.save(withdrawInfo);
