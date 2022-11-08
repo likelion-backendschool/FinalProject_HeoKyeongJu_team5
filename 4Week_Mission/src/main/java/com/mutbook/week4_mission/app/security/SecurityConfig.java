fcd26576630587bf8a4fa6ce4638e17d44b7b682
heokyeongju <beewt@naver.com>
2022-11-08 17:35:45 +0900

    feat: swagger 관련 security 설정 #20

--- 4Week_Mission/src/main/java/com/mutbook/week4_mission/app/security/SecurityConfig.java
+++ 4Week_Mission/src/main/java/com/mutbook/week4_mission/app/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.mutbook.week4_mission.app.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,7 +9,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
@@ -21,6 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .httpBasic().disable()
                 .formLogin(
                         formLogin -> formLogin
                                 .loginPage("/member/login") // GET
@@ -28,8 +27,16 @@ public class SecurityConfig {
                                 .successHandler(authenticationSuccessHandler)
                                 .failureHandler(authenticationFailureHandler)
                 )
+                .authorizeRequests(
+                        authorizeRequests -> authorizeRequests
+                                .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
+                                .hasAuthority("ADMIN")
+                                .anyRequest()
+                                .permitAll()
+                )
                 .logout(
-                        logout -> logout.logoutUrl("/member/logout")
+                        logout -> logout
+                                .logoutUrl("/member/logout")
                 );
 
         return http.build();
